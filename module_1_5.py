9f6ba59caf5119f2a390f8ec2430ea2262945d35
Yukie765 <yliya-kate@mail.ru>
2024-08-20 23:44:01 +0500

    Update module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -4,7 +4,7 @@ print(immutable_var)
 #Элементы кортежа невозможно изменить, так как кортеж является неизменяемым объектом
 #то есть значения элементов не могут быть изменены после того, как были созданы
 
-mutable_list = 1, 2, True, [4, 5]
+mutable_list = [1, 2, True, [4, 5]]
 print(mutable_list)
 mutable_list[3][1] = 'y'
 print(mutable_list)
